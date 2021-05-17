7c803953ab8baff15fc1c77cfa65ed764bed3d4b
John <44213272+protonlaser91@users.noreply.github.com>
2021-05-16 22:22:11 -0400

    Creating Graph class
    Graph class constructor and draw method being worked upon.

--- src/storage/Graph.java
+++ src/storage/Graph.java
@@ -2,28 +2,67 @@ package storage;
 
 import core.Applet;
 import geom.Grid;
+import processing.core.PShape;
+import processing.core.PVector;
 
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+import static geom.Grid.HEIGHT;
+import static geom.Grid.WIDTH;
+import static processing.core.PConstants.HSB;
+import static processing.core.PConstants.LINES;
 
 public class Graph { // Must store x values separately, this only holds y values
     // for efficiency!
-    public float[] pointValues;
+    public static double[] xValues; // Set them here, but Graph methods do not touch this!
+    public double[] pointValues;
     public Color color;
     public String name;
+    public Grid grid;
+    private static float distance = 1; // dont want to make final
 
-    public Graph(float[] pv){
+    public Graph(Grid grid, double[] pv, Color color, String name){
+        this.grid = grid
         this.pointValues = pv;
+        this.color = color;
+        this.name = name;
     }
 
-    public Graph(Function<Double, Double> function){
-
+    public Graph(Grid grid, Function<Double, Double> function, Color color){
+       // this.p = grid.getProcessingInstance();
+        //this.pointValues = IntStream.range(0, (int) (8/distance)).asDoubleStream().map(t -> function.apply(t*distance)).toArray();
+        this(grid,IntStream.range(0, (int) (8/distance)).asDoubleStream().map(t -> function.apply(t*distance)).toArray(),color);
+        xValues = IntStream.range(0,(int) (8/distance)).asDoubleStream().toArray();
+        System.out.println(Arrays.toString(pointValues));
     }
 
-    public void draw(int index, Grid grid){
+    public Graph(Grid grid, double[] pv, Color color){
+        this(grid,pv,color,"");
+    }
+
+    public void draw(){
+        p.stroke(color);
+    }
+
+    private PShape createGraphShape(){
         Applet p = grid.getProcessingInstance();
+        PVector displacement = grid.getDisplacement();
+        PShape shape = p.createShape();
+        shape.colorMode(HSB);
+        shape.stroke(color.getHue().getValue(),color.getBrightness().getValue(),color.getSaturation().getValue(),color.getAlpha().getValue());
+        shape.beginShape(LINES);
+        for (int i = 1; i < xValues.length; i++){
+            shape.vertex(displacement.x + (float) xValues[i-1],(float) pointValues[i-1]);
+            shape.vertex(displacement.y + (float) xValues[i],(float) pointValues[i]);
+        }
+        shape.endShape();
+        return shape;
     }
 
-    public void setPointValues(float[] pv){
+    public void setPointValues(double[] pv){
         this.pointValues = pv;
     }
 
